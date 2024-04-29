dec8f86043014b17e23c6e07d1dfbd25b6a66b73
laomao <43214386@qq.com>
2024-04-29 15:35:25 +0800

    first

---
+++ lm-main/src/main/java/com/laomaodiaoyu/lmtiny/lmmain/LmMainApplication.java
@@ -0,0 +1,13 @@
+package com.laomaodiaoyu.lmtiny.lmmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmMainApplication.class, args);
+    }
+
+}
